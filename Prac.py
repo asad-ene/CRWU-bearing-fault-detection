69c46782c5948f7b925dc91e432fed1e5293ad0d
asad-ene <139612301+asad-ene@users.noreply.github.com>
2026-06-02 11:13:51 +0500

    with hyperparameters

--- Prac.py
+++ Prac.py
@@ -8,21 +8,21 @@ from scipy.stats import skew, kurtosis
 
 
 df = pd.read_csv('D:/Python and ML/CRWU bearing/test.csv')
-df['peak_to_peak'] = df['max'] - df['min']
-df['crest_factor'] = df['max'] / (df['rms'] + 1e-6)
-df['shape_factor'] = df['rms'] / (df['mean'].abs() + 1e-6)
-df['impulse_factor'] = df['max'] / (df['mean'].abs() + 1e-6)
-df['margin_factor'] = df['max'] / (df['sd'] + 1e-6)
-df['skewness_kurtosis'] = df['skewness'] * df['kurtosis']
-df['rms_sd_ratio'] = df['rms'] / (df['sd'] + 1e-6)
 
 X = df.drop('fault', axis=1)
-Y = df['fault']
+y = df['fault']
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
-model = RandomForestClassifier(n_estimators=100, random_state=42)
-model.fit(X_train, Y_train)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+model = RandomForestClassifier(
+        n_estimators=200,
+        criterion='entropy',
+        min_samples_split=5,
+        max_depth=20,
+        random_state=42
+)
+model.fit(X_train, y_train)
 
-Y_pred = model.predict(X_test)
-print(classification_report(Y_test, Y_pred))
-print(confusion_matrix(Y_test, Y_pred))
+y_pred = model.predict(X_test)
+print(classification_report(y_test, y_pred))
+print(confusion_matrix(y_test, y_pred))
+print("accuracy = ", model.score(X_test, y_test))
